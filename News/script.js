2765925e88ba58aefecb92738aa82ea4f9151e3f
Erfan Khedmati <100248103+EranKhedmati@users.noreply.github.com>
2022-07-15 23:13:06 +0430

    add slider to home

---
+++ News/script.js
@@ -0,0 +1,36 @@
+// Slider
+let sliderImages = ['../src/slider imgae 1.jpg', '../src/slider imgae 2.jpg'];
+let sliderSelector = 0;
+
+let sliderContent = document.getElementById('sliderContent');
+
+let perviousSlider = document.getElementById('perviousSlider');
+let nextSlider = document.getElementById('nextSlider');
+
+nextSlider.addEventListener('click', () => {
+    sliderSelector++;
+    if (sliderSelector > sliderImages.length - 1) {
+        sliderSelector = 0;
+    }
+    sliderContent.src = sliderImages[sliderSelector];
+})
+
+perviousSlider.addEventListener('click', () => {
+    sliderSelector--;
+    if (sliderSelector < 0) {
+        sliderSelector = sliderImages.length - 1;
+    }
+    sliderContent.src = sliderImages[sliderSelector];
+})
+
+function autoChangeSlider() {
+    sliderSelector++;
+    if (sliderSelector > sliderImages.length - 1) {
+        sliderSelector = 0;
+    }
+    sliderContent.src = sliderImages[sliderSelector];
+}
+
+// const setChangeSlider = setInterval(autoChangeSlider, 3000);
+
+sliderContent.src = sliderImages[sliderSelector];
